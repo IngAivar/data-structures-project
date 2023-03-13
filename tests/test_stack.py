9f031cb2dca3a68e696bf32dae1f92a366a2f26b
MiniGan067 <MiniGan067@yandex.ru>
2023-03-13 06:10:27 +0300

    Update project

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,7 +1,7 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
 
 import unittest
-from src.stack import Stack
+from src.stack import Node, Stack
 
 
 class TestStack(unittest.TestCase):
@@ -23,3 +23,17 @@ class TestStack(unittest.TestCase):
         self.assertEqual(stack.top.next_node.data, 'data2')
         self.assertEqual(stack.top.next_node.next_node.data, 'data3')
         self.assertEqual(stack.top.next_node.next_node.next_node, None)
+
+    def test_stack_pop(self):
+        stack = Stack()
+        stack.push('data1')
+        stack.push('data2')
+        stack.push('data3')
+        data3 = stack.pop()
+        data2 = stack.pop()
+        data1 = stack.pop()
+        data = stack.pop()
+        self.assertEqual(data3, "data3")
+        self.assertEqual(data2, "data2")
+        self.assertEqual(data1, "data1")
+        self.assertEqual(data, None)
