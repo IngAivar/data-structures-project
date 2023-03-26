e0b71569992b8028709a45f7e552d62b48ca4b6c
MiniGan067 <MiniGan067@yandex.ru>
2023-03-26 15:13:06 +0300

    Update project. homework 5

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,21 +1,62 @@
 class Node:
-    """Класс для узла односвязного списка"""
-    pass
+    """
+    Класс для узла односвязного списка
+    """
+
+    def __init__(self, data, next_node=None):
+        """
+        Инициализатор класса Node
+        """
+
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
-    """Класс для односвязного списка"""
+    """
+    Класс для односвязного списка
+    """
+
+    def __init__(self):
+        """
+        Инициализатор класса LinkedList
+        """
+
+        self.head = None
+        self.tail = None
 
     def insert_beginning(self, data: dict) -> None:
-        """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        """
+        Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка
+        """
+
+        node = Node(data)
+
+        if self.head is None:
+            self.head = node
+            self.tail = node
+        else:
+            node.next_node = self.head
+            self.head = node
 
     def insert_at_end(self, data: dict) -> None:
-        """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        """
+        Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка
+        """
+
+        node = Node(data)
+
+        if self.head is None:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next_node = node
+            self.tail = node
 
     def __str__(self) -> str:
-        """Вывод данных односвязного списка в строковом представлении"""
+        """
+        Вывод данных односвязного списка в строковом представлении
+        """
         node = self.head
         if node is None:
             return str(None)
